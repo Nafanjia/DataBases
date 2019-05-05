dbb79223d981377dd3682090ab9cd516b3c0c995
Nafanjia <infantium@yandex.ru>
2019-05-05 19:08:30 +0300

    1

--- 3_1.sql
+++
@@ -1,6 +0,0 @@
-/*
-Пусть в таблице users поля created_at и updated_at оказались незаполненными. Заполните их текущими датой и временем.
-*/
-update users
-set created_at = now(), updated_at = now()
-where created_at is null and updated_at is null
